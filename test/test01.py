8a11f399c2381b63aef8d8323a33394a77d0521d
Nicol-Huaiquil <72150599+Nicol-Huaiquil@users.noreply.github.com>
2023-07-04 15:43:05 -0400

    añadiendo ejemplo

---
+++ test/test01.py
@@ -0,0 +1,10 @@
+import os
+import sys
+# Add the parent directory of mypackage to the Python path
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
+# Import subpackage.submodule
+
+from app1 import *
+
+
+Apps.funLVC()
